46357d2279f4398ff4d1e5f5d230c87739187667
SharathgowdaDR <sharathgowdadr63>
2023-07-07 19:02:17 +0530

    hd

--- dom.js
+++ dom.js
@@ -1,9 +1,4 @@
-var newDiv = document.createElement('div');
-var newDivtext = document.createTextNode('HelloWorld');
-newDiv.appendChild(newDivText);
-var container = document.querySelector('header.container');
-var h1 = document.querySeelector('header h1');
-console.log(newDiv);
-newDiv.style.fontsize = '30px';
-container.insertBefore(newDiv,h1);
-container.insertBefore(newDiv,h2);
+var itemList = document.getElementById('items');
+li.appendChild(editbtn);
+
+ 
