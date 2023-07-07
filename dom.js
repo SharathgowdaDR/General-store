32e54ef68c410eac5b27d3f3aa6e6617178abc77
SharathgowdaDR <sharathgowdadr63>
2023-07-07 19:45:27 +0530

    hdd

--- dom.js
+++ dom.js
@@ -1,4 +1,12 @@
-var itemList = document.getElementById('items');
-li.appendChild(editbtn);
-
- 
+function filterItems(e){
+    var text = e.target.value.toLowerCase();
+    var items = itemList.getElementsByTagName('li');
+    Array.from(items).forEach(function(item){
+      var itemName = item.firstChild.textContent;
+      if(itemName.toLowerCase().indexOf(text) != -1){
+        item.style.display = 'block';
+      } else {
+        item.style.display = 'none';
+      }
+    });
+  }
