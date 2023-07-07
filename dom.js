c541e8179dfb4c3ea0da2a171d122afab8e30fbf
SharathgowdaDR <sharathgowdadr63>
2023-07-07 11:18:54 +0530

    ht commit

--- dom.js
+++ dom.js
@@ -1,10 +1,9 @@
-var seconditem = document.querySelector('.list-group-item:nthchild(2)')
-secondItem.style.backgroundcolor = 'red';
-var thirditem = document.querySelector('.list-group-item:nthchild(3)')
-thirdItem='Invisible';
-var seconditem = document.querySelectorAll('.list-group-item:nthchild(2)')
-secondItem.style.color = 'green';
-var odd = document.querySelectorAll('li-nth-child(odd');
-for(var i=0;i<odd.length;i++){
-    odd[i].style.backgroundcolor = 'green';
-}
+var newDiv = document.createElement('div');
+var newDivtext = document.createTextNode('HelloWorld');
+newDiv.appendChild(newDivText);
+var container = document.querySelector('header.container');
+var h1 = document.querySeelector('header h1');
+console.log(newDiv);
+newDiv.style.fontsize = '30px';
+container.insertBefore(newDiv,h1);
+container.insertBefore(newDiv,h2);
