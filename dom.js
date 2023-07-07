c81049c0fb80d7c5f5f2faf760b239b4431a6059
SharathgowdaDR <sharathgowdadr63>
2023-07-07 10:41:28 +0530

    hf

---
+++ dom.js
@@ -0,0 +1,10 @@
+var seconditem = document.querySelector('.list-group-item:nthchild(2)')
+secondItem.style.backgroundcolor = 'red';
+var thirditem = document.querySelector('.list-group-item:nthchild(3)')
+thirdItem='Invisible';
+var seconditem = document.querySelectorAll('.list-group-item:nthchild(2)')
+secondItem.style.color = 'green';
+var odd = document.querySelectorAll('li-nth-child(odd');
+for(var i=0;i<odd.length;i++){
+    odd[i].style.backgroundcolor = 'green';
+}
